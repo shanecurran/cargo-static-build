49b639236d55c7d9237171409322ed1864a9c67b
Shane Curran <shane@curran.ie>
2021-01-18 18:20:13 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -l
 
 # hack, move home to $HOME(/github/home)
-# ln -s /home/ec2-user/.cargo $HOME/.cargo
-# ln -s /home/ec2-user/.rustup $HOME/.rustup
+ln -s /home/ec2-user/.cargo $HOME/.cargo
+ln -s /home/ec2-user/.rustup $HOME/.rustup
 
 # go to the repo root
 cd $GITHUB_WORKSPACE
